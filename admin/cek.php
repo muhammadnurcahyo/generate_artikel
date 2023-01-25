807f6f4e9007782a610530878668a29bada5b38d
muhammadnurcahyo <mukhamad.c@students.amikom.ac.id>
2023-01-25 16:41:06 +0700

    update login2

--- admin/cek.php
+++ admin/cek.php
@@ -1,9 +1,6 @@
 <?php
 //jika belum login
-<<<<<<< HEAD
 
-=======
->>>>>>> 0e8c5de28a240c2c481cf5bb99074eb3ff9311f0
 if(isset($_SESSION['log'])){
 
  }else{
--- admin/logout.php
+++ admin/logout.php
@@ -2,8 +2,4 @@
 session_start();
 session_destroy();
 header('location:login.php');
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e8c5de28a240c2c481cf5bb99074eb3ff9311f0
 ?>
